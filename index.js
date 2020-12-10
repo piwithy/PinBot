ea95f02d4d017b67ee62e03b124985561738b6c5
Jezegou Pierre-Yves <salydu29@gmail.com>
2020-12-10 16:40:54 +0100

    worked on bot

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+})
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    if (user.bot) return;
+    const {message, emoji} = messageReaction;
+    if (emoji.name === "📌"){
+        console.log(`User: ${user.username} Requested to pin a message in ${message.guild.name}`);
+        message.pin();
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
